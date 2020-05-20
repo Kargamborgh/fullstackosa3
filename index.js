19dbcd1f935b7f98ca66a5884cbec5fb304d2307
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-21 00:16:49 +0300

    3.1 tehty

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Kari Taalasmaa",
+        number: "0700-123123",
+        id: 2
+    },
+    {
+        name: "Urho Kekkonen",
+        number: "050-456789",
+        id: 3
+    },
+    {
+        name: "Aku Ankka",
+        number: "176-671",
+        id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Puhelinluettelo ebin</h1>')
+  })
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
