be89cf1fa892fd9a3aff748eaca4392a2648af0b
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-21 19:58:23 +0300

    3.11 build .gitignore unohtui, nyt on sekin kunnossa

---
+++ build/precache-manifest.ade7e8f8ee03d9549ca80e92807320e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162e6c5050f13be6dc1734beaaab13dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df317c0502d469c768de",
+    "url": "/static/css/main.399ccbc8.chunk.css"
+  },
+  {
+    "revision": "f5f7b13d34d14b56302e",
+    "url": "/static/js/2.076799b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.076799b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df317c0502d469c768de",
+    "url": "/static/js/main.cb98dd44.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
