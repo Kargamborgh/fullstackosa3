9ef67cfddd3be8ba802b20bd09eb3ca71f24d139
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-23 13:56:34 +0300

    3.19 tehty

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/info', (req, res) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (body.name === undefined) {
@@ -63,10 +63,14 @@ app.post('/api/persons', (request, response) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
+  person
+  .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
+    })
+    .catch(error => next(error))
   })
-})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
@@ -104,7 +108,11 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name.kind === 'mongoose-unique-validator') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
